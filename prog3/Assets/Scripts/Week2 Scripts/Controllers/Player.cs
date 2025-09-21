45b300d0aaa224f37dd82abecf0c42b19c4d479e
iz-mi <wangmic@sheridancollege.ca>
2025-09-21 15:04:03 -0400

    vectors in space Task A
    - organized scenes and scripts into folders sorted by week
    
    - task A complete

---
+++ prog3/Assets/Scripts/Week2 Scripts/Controllers/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Transform enemyTransform;
+    public GameObject bombPrefab;
+    public List<Transform> asteroidTransforms;
+
+    public Vector2 bombOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            SpawnBombAtOffset(bombOffset);
+        }
+    }
+
+    public void SpawnBombAtOffset(Vector3 inOffset)
+    {
+        Instantiate(bombPrefab, bombOffset, Quaternion.identity);
+    }
+}
