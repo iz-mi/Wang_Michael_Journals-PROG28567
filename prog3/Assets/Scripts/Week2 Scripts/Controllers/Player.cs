b125b057f264226025485f953a13404c1972bea0
iz-mi <wangmic@sheridancollege.ca>
2025-09-24 21:39:29 -0400

    Task B Bomb Trail

--- prog3/Assets/Scripts/Week2 Scripts/Controllers/Player.cs
+++ prog3/Assets/Scripts/Week2 Scripts/Controllers/Player.cs
@@ -9,17 +9,43 @@ public class Player : MonoBehaviour
 
     public Vector2 bombOffset;
 
+    public float bombTrailSpacing = 5f;
+    public int numberOfTrailBombs = 5;
+
     // Update is called once per frame
     void Update()
     {
+        //check for B input, then execute SpawnBombAtOffset
         if (Input.GetKeyDown(KeyCode.B))
         {
             SpawnBombAtOffset(bombOffset);
         }
+
+        //check for T input, then execute SpawnBombTrail
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs)
+        }
     }
 
+    //task A
     public void SpawnBombAtOffset(Vector3 inOffset)
     {
+        //set offset relative to player's location
+        Vector3 bombOffset = transform.position + inOffset;
+
+        //instantiate prefab
         Instantiate(bombPrefab, bombOffset, Quaternion.identity);
     }
+
+    //task B
+    public void SpawnBombTrail(float inBombSPacing, int inNumberOfBombs)
+    { 
+        for (int i = 1; i <= inNumberOfBombs) ;
+        {
+            Vector3 offset = new Vector3();
+            Vector3 bombSpawnPos = transform.position + inOffset
+        }
+    }
+
 }
