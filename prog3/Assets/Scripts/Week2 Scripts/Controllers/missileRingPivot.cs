3b0695c61a8f31b3f786bd4027669f1223891809
iz-mi <wangmic@sheridancollege.ca>
2025-10-20 07:30:06 -0400

    Space Shooter Final

--- prog3/Assets/Scripts/Week2 Scripts/Controllers/missileRingPivot.cs
+++ prog3/Assets/Scripts/Week2 Scripts/Controllers/missileRingPivot.cs
@@ -4,7 +4,9 @@ public class missileRingPivot : MonoBehaviour
 {
     public float rotationSpeed = 180f;
     public float ringLifetime = 2f;
+    public float movespeed = 2f;
 
+    public GameObject playerPrefab;
     float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -18,6 +20,10 @@ public class missileRingPivot : MonoBehaviour
     {
         //rotate the pivot point
         transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+
+        //follow player prefab
+        Vector3 direction = (playerPrefab.transform.position - transform.position).normalized;
+        transform.position += direction * movespeed * Time.deltaTime;
 
         //destroy missile ring after set duration
         timer -= Time.deltaTime;
